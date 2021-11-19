7879c1230a13d8e0d8bc383904e5f044eadc1692
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-19 16:44:31 -0500

    added color-coded logic

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function initPage() {
   var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
   sevenPm.val(init7);
 }
+
+function background () {
+      
+  $(".form-control").each(function () {
+    var timeTest = parseInt($(this).attr("id"));
+    hour = parseInt(hour);
+    console.log(timeTest);
+    console.log(hour);
+//      console.log(this);
+    if (hour > timeTest) {
+        $(this).addClass("past");
+    } else if (hour < timeTest) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("present");
+    }
+});
+}
+
+$(document).ready(function(){
+  initPage()
+  background()
+}); 
