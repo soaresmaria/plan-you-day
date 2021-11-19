46e137348641c61891f3e44df4c486e076ec8415
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-19 16:33:14 -0500

    added time blocks function for standard business hours

--- script.js
+++ script.js
@@ -26,6 +26,38 @@ var interval = setInterval(function() {
 }, 100);
 
 
-function initPage() {}
+function initPage() {
+  console.log("Current Hour " + hour);
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(init9);
+
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(init10);
   
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(init11);
   
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(init12);
+  
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(init1);
+  
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(init2);
+  
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(init3);
+ 
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(init4);
+  
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(init5);
+  
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+  sixPm.val(init6);
+  
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+  sevenPm.val(init7);
+}
