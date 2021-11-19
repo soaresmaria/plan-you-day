298abeab195b61f54a32501a98afbda715740506
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-19 03:42:38 -0500

    added time blocks

---
+++ script.js
@@ -0,0 +1,17 @@
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+
+
+function initPage() {}
